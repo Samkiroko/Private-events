5421d7bb0ad7051732291aeac3cd5dc6faa6f80f
Samkiroko <kirokodev@gmail.com>
2020-06-15 12:33:45 +0300

    refoctoring the view logic issues and updating test with spec

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,4 +6,12 @@ module SessionsHelper
   def logged_in?
     current_user
   end
+
+  def render_links
+    if signed?
+      render partial: 'layouts/links_signed'
+    else
+      render partial: 'layouts/links_not_signed'
+    end
+  end
 end
